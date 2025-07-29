489c4753d0229bd9dc3f1c65c39f2b206c8ebfc5
Andrea Martina <andrea.martina1@studenti.unimi.it>
2025-07-30 01:44:46 +0200

    typo

--- js/cards.js
+++ js/cards.js
@@ -116,7 +116,7 @@ function renderCards() {
           </div>
           <span class="card__vat">${card.vat}</span>
         </div>
-        <button class="card__btn">Details <span class="card__btn-arrow" style="display:inline-flex;vertical-align:middle;"><svg xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg></span></button>
+        <button class="card__btn">Dettagli<span class="card__btn-arrow" style="display:inline-flex;vertical-align:middle;"><svg xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg></span></button>
       </div>
     `;
     grid.appendChild(el);
